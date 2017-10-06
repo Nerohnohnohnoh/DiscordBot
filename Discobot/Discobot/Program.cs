afee74a115861f5c058ae40a51e7122197b64913
mateusz.majkowski <mateusz.majkowski@gmail.com>
2017-10-06 22:03:32 +0200

    Project start

---
+++ Discobot/Discobot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Discobot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
