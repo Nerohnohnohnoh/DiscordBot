8552c424e62c3c553da023189aeb8ffdae82c9ad
mateusz.majkowski <mateusz.majkowski@gmail.com>
2017-10-06 22:09:38 +0200

    Migrating project

---
+++ Discobot/Discobot/Modules/Test.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    public class Test : ModuleBase<SocketCommandContext>
+    {
+        [Command("Test")]
+        public async Task tescik(String repeat)
+        {
+            await Context.Channel.SendMessageAsync(repeat);
+        }
+    }
+}
--- Discobot/Discobot/Program.cs
+++ Discobot/Discobot/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using DiscoBot;
+using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
 
 namespace Discobot
 {
-    class Program
+    public class Program
     {
+        private DiscordSocketClient _client;
+        private CommandHandler _handler;
+
         static void Main(string[] args)
+            => new Program().StartAsync().GetAwaiter().GetResult();
+
+        public async Task StartAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+            await _client.LoginAsync(TokenType.Bot, "MzY1NjE1NTcxMzg0OTI2MjE5.DLkkig.dRLVM5wx99k3y8duxr1I5xL2UNk");
+            await _client.StartAsync();
+            _handler = new CommandHandler(_client);
+            await Task.Delay(-1);
+
+
         }
     }
 }
